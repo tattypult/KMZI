dab391d6a0d044c90fe8ce575df45020e0619bfa
nuyur <93263632+tik1tac@users.noreply.github.com>
2022-05-21 21:47:12 +0300

    pochti

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SRC_IP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SRC_Port = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DST_IP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DST_Port = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 401);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(895, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 327);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(895, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Location = new System.Drawing.Point(0, 351);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(895, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Начать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(895, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SRC_IP,
+            this.SRC_Port,
+            this.DST_IP,
+            this.DST_Port});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(895, 287);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // SRC_IP
+            // 
+            this.SRC_IP.HeaderText = "SRC";
+            this.SRC_IP.MinimumWidth = 6;
+            this.SRC_IP.Name = "SRC_IP";
+            this.SRC_IP.Width = 125;
+            // 
+            // SRC_Port
+            // 
+            this.SRC_Port.HeaderText = "SRCP";
+            this.SRC_Port.MinimumWidth = 6;
+            this.SRC_Port.Name = "SRC_Port";
+            this.SRC_Port.Width = 125;
+            // 
+            // DST_IP
+            // 
+            this.DST_IP.HeaderText = "DST";
+            this.DST_IP.MinimumWidth = 6;
+            this.DST_IP.Name = "DST_IP";
+            this.DST_IP.Width = 125;
+            // 
+            // DST_Port
+            // 
+            this.DST_Port.HeaderText = "DSTP";
+            this.DST_Port.MinimumWidth = 6;
+            this.DST_Port.Name = "DST_Port";
+            this.DST_Port.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 431);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SRC_IP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SRC_Port;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DST_IP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DST_Port;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using Devart.Data.SQLite;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        mainEntities main = new mainEntities();
+        FIREWALL fire;
+        KASPERSKY kaspersky = new KASPERSKY();
+        USB usb = new USB();
+        string path = "C:\\Users\\user\\Desktop\\WindowsFormsApplication1\\WindowsFormsApplication1\\data.txt";
+
+        string[] s;
+        string[] s2;
+        public Form1()
+        {
+            InitializeComponent();
+            s = File.ReadAllLines(path);
+            s2 = new string[s.Length * 4];
+            dataGridView1.Rows.Add(s.Length);
+            for (int i = 0; i < s.Length; i++)
+            {
+                s2 = s[i].Split('^');
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = s2[j];
+                }
+            }
+
+            fire = new FIREWALL();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter str = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        if (j != dataGridView1.Columns.Count - 1)
+                            str.Write(dataGridView1.Rows[i].Cells[j].Value + "^");
+                        else
+                            str.Write(dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                    if (i != dataGridView1.Rows.Count - 2)
+                        str.Write("\n");
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string mySelectQuery = "SELECT SRC_IP FROM FIREWALL";
+            //string myConnString = "DataSource=DB.db;";
+            //SQLiteConnection sqConnection = new SQLiteConnection(myConnString);
+            //SQLiteCommand sqCommand = new SQLiteCommand(mySelectQuery, sqConnection);
+            //sqConnection.Open();
+            //SQLiteDataReader sqReader = sqCommand.ExecuteReader();
+            //var enumer= sqReader.;
+            //for (int i = 0; sqReader.Read(); i++)
+            //{
+
+            //    if (enumer == listBox1.Items[3])
+            //        MessageBox.Show("Alarm Пиздец");
+            //}
+            //sqReader.Close();
+            //// always call Close when done reading.
+            //sqConnection.Close();
+
+            //Написать проверку каждого столбца и проверки добавления новых данных
+
+            //foreach (var item in main.FIREWALL)
+            //{
+            //    Parallel.Invoke(
+            //        () =>
+            //        {
+            //            for (int i = 0; i < listBox1.Items.Count; i++)
+            //            {
+            //                if (item.SRC_IP == listBox1.Items[3].ToString())
+            //                {
+            //                    MessageBox.Show("Alarm Пизда");
+            //                }
+            //            }
+            //        },
+            //        () =>
+            //        {
+            //            for (int i = 0; i < listBox1.Items.Count; i++)
+            //            {
+            //                if (item.SRC_PORT == listBox1.Items[3].ToString())
+            //                {
+            //                    MessageBox.Show("Alarm Пизда");
+            //                }
+            //            }
+            //        },
+            //         () =>
+            //        {
+            //            for (int i = 0; i < listBox1.Items.Count; i++)
+            //            {
+            //                if (item.DST_IP == listBox1.Items[3].ToString())
+            //                {
+            //                    MessageBox.Show("Alarm Пизда");
+            //                }
+            //            }
+            //        },
+            //        () =>
+            //        {
+            //            for (int i = 0; i < listBox1.Items.Count; i++)
+            //            {
+            //                if (item.DST_PORT == listBox1.Items[3].ToString())
+            //                {
+            //                    MessageBox.Show("Alarm Пизда");
+            //                }
+            //            }
+            //        });
+            //}
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
